03b64b3fedf4351c2bd99ebad2ed27a214942a44
adnope <anhduyxh7@gmail.com>
2024-10-07 00:41:58 +0700

    Implemented FXML and CSS for the main menu, no logic added

--- app/src/main/java/org/tomfoolery/configurations/monolith/gui/MainApplication.java
+++ app/src/main/java/org/tomfoolery/configurations/monolith/gui/MainApplication.java
@@ -1,19 +1,11 @@
 package org.tomfoolery.configurations.monolith.gui;
 
-import org.tomfoolery.configurations.monolith.gui.view.Components.MenuLogo;
-import org.tomfoolery.configurations.monolith.gui.view.Components.NavBar;
-import org.tomfoolery.configurations.monolith.gui.view.Components.ProfileIcon;
-import org.tomfoolery.configurations.monolith.gui.view.Components.SearchBar;
-import org.tomfoolery.configurations.monolith.gui.view.Components.SidebarComponents;
-
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
@@ -23,42 +15,16 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        VBox root = new VBox();
-        Scene scene = new Scene(root);
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainMenu.fxml"));
+        Scene scene = new Scene(root, Color.LIGHTBLUE);
 
-        MenuLogo menuLogo = new MenuLogo("/menu_logo.png", 40, 230);
-        SearchBar searchBar = new SearchBar("/search_icon.png", 500, 60);
-        ProfileIcon profileIcon = new ProfileIcon("/profile_icon.png", 60);
-
-        NavBar navbar = new NavBar(80, 10, 10, menuLogo.getLogo(), searchBar.getSearchBar(), profileIcon.getIcon());
-
-        HBox belowNavbar = new HBox();
-        VBox.setVgrow(belowNavbar, Priority.ALWAYS);
-        belowNavbar.setStyle("-fx-background-color: orange");
-
-        VBox sidebar = new VBox();
-        sidebar.setPrefWidth(300);
-        sidebar.setAlignment(Pos.CENTER);
-        sidebar.setSpacing(40);
-        HBox.setMargin(sidebar, new Insets(20, 0, 0, 20));
-        sidebar.setPadding(new Insets(20));
-        sidebar.setStyle("-fx-background-color: lightgray; " +
-                "-fx-background-radius: 15; " +
-                "-fx-padding: 10;");
-        SidebarComponents sidebarComponents = new SidebarComponents();
-        sidebar.getChildren().addAll(sidebarComponents.getCategory(), sidebarComponents.getContact(),
-                sidebarComponents.getDashboard());
-
-        belowNavbar.getChildren().addAll(sidebar);
-        VBox.setVgrow(sidebar, Priority.ALWAYS);
-
-        root.getChildren().addAll(navbar.getNavBar(), belowNavbar);
-
-        Image appIcon = new Image(getClass().getResourceAsStream("/logo.png"));
-        stage.getIcons().add(appIcon);
-        stage.setHeight(900);
+        stage.setMinHeight(720);
+        stage.setMinWidth(1280);
         stage.setWidth(1600);
-        stage.setTitle("Tomfoolery");
+        stage.setHeight(900);
+        Image icon = new Image(getClass().getResourceAsStream("/logo.png"));
+        stage.getIcons().add(icon);
+        stage.setTitle("Tomfoolery - Library Management App");
         stage.setScene(scene);
         stage.show();
     }
