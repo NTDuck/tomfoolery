3ab64be83b76210e79a4dd1de44aec322331617d
adnope <anhduyxh7@gmail.com>
2024-10-15 04:38:15 +0700

    I spent 6 hours for absolutely nothing done

--- app/src/main/java/org/tomfoolery/configurations/monolith/gui/MainApplication.java
+++ app/src/main/java/org/tomfoolery/configurations/monolith/gui/MainApplication.java
@@ -4,11 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import lombok.NonNull;
 
 public class MainApplication extends Application {
     public static void main(String[] args) {
@@ -17,14 +14,14 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainMenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
         Scene scene = new Scene(root);
 
         stage.setMinHeight(720);
         stage.setMinWidth(1280);
         stage.setWidth(1600);
         stage.setHeight(900);
-        Image icon = new Image(getClass().getResourceAsStream("/logo.png"));
+        Image icon = new Image(getClass().getResourceAsStream("/images/logo.png"));
         stage.getIcons().add(icon);
         stage.setTitle("Tomfoolery - Library Management App");
         stage.setScene(scene);
--- app/src/main/java/org/tomfoolery/configurations/monolith/gui/controller/MainMenuController.java
+++ app/src/main/java/org/tomfoolery/configurations/monolith/gui/controller/MainMenuController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+
 import java.io.IOException;
 
 public class MainMenuController {
@@ -20,13 +21,13 @@ public class MainMenuController {
     public void initialize() {
         loadNavigationBar();
         loadSidebar();
-        loadContent("/fxml/Discover.fxml");
+        loadContent("/view/Dashboard.fxml");
     }
 
     @FXML
     private void loadSidebar() {
         try {
-            VBox side = FXMLLoader.load(getClass().getResource("/fxml/Sidebar.fxml"));
+            VBox side = FXMLLoader.load(getClass().getResource("/view/Sidebar.fxml"));
             sidebar.getChildren().setAll(side);
         } catch (IOException e) {
             e.printStackTrace();
@@ -36,7 +37,7 @@ public class MainMenuController {
     @FXML
     private void loadNavigationBar() {
         try {
-            HBox nav = FXMLLoader.load(getClass().getResource("/fxml/NavigationBar.fxml"));
+            HBox nav = FXMLLoader.load(getClass().getResource("/view/NavigationBar.fxml"));
             navigationBar.getChildren().setAll(nav);
         } catch (IOException e) {
             e.printStackTrace();
