770b85e3bc08bb13152f907d42e67971bb2d43fa
FearOfTheSea <ptrunghieu0312@gmail.com>
2024-11-03 12:16:01 +0700

    Initial cloud database and API setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,14 @@ dependencies {
 
     // Used by `application`
     implementation(libs.guava)
+
+    // Used for Google Sheets API and Authentication
+    implementation("com.google.apis:google-api-services-sheets:v4-rev20220927-2.0.0")
+    implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
+    implementation("com.google.api-client:google-api-client:2.0.0")
+    implementation("com.google.auth:google-auth-library-oauth2-http:1.19.0")
+    implementation("com.google.http-client:google-http-client-jackson2:1.43.3")
+    implementation("com.google.oauth-client:google-oauth-client:1.34.1")
 }
 
 group = "org.tomfoolery"
---
+++ app/src/main/java/org/tomfoolery/infrastructures/dataproviders/cloud/GoogleSheetsConfig.java
@@ -0,0 +1,72 @@
+package org.tomfoolery.infrastructures.dataproviders.cloud;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+
+import lombok.NoArgsConstructor;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.io.*;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+public class GoogleSheetsConfig {
+    private static final String APPLICATION_NAME = "tomfoolery";
+    private static final String SPREADSHEET_ID = "1djV8X6qqdhfcq--uQAQl9els71SjWp9Ii9LlnJSHg8Y";
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+
+    private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
+
+    @NonNull
+    private static Credential getCredentials(@NonNull final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        InputStream in = GoogleSheetsConfig.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+
+        @NonNull
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
+                JSON_FACTORY, new InputStreamReader(in));
+
+        @NonNull
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+
+        @NonNull
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    @NonNull
+    public static Sheets getSheetsService() throws IOException, GeneralSecurityException {
+        @NonNull
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+
+        return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    @NonNull
+    public static String getSpreadsheetId() {
+        return SPREADSHEET_ID;
+    }
+}
