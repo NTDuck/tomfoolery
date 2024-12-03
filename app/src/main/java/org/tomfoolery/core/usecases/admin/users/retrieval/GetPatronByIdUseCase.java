7f19f3acf9713913c280768abac5ba75ecc526fe
NTDuck <nguyentuduck@gmail.com>
2024-12-03 10:37:29 +0700

    Finalized use cases, probably

---
+++ app/src/main/java/org/tomfoolery/core/usecases/admin/users/retrieval/GetPatronByIdUseCase.java
@@ -0,0 +1,18 @@
+package org.tomfoolery.core.usecases.admin.users.retrieval;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.tomfoolery.core.dataproviders.generators.users.auth.security.AuthenticationTokenGenerator;
+import org.tomfoolery.core.dataproviders.repositories.users.PatronRepository;
+import org.tomfoolery.core.dataproviders.repositories.users.security.AuthenticationTokenRepository;
+import org.tomfoolery.core.domain.users.Patron;
+import org.tomfoolery.core.usecases.admin.users.retrieval.abc.GetUserByIdUseCase;
+
+public final class GetPatronByIdUseCase extends GetUserByIdUseCase<Patron> {
+    public static @NonNull GetPatronByIdUseCase of(@NonNull PatronRepository patronRepository, @NonNull AuthenticationTokenGenerator authenticationTokenGenerator, @NonNull AuthenticationTokenRepository authenticationTokenRepository) {
+        return new GetPatronByIdUseCase(patronRepository, authenticationTokenGenerator, authenticationTokenRepository);
+    }
+
+    private GetPatronByIdUseCase(@NonNull PatronRepository patronRepository, @NonNull AuthenticationTokenGenerator authenticationTokenGenerator, @NonNull AuthenticationTokenRepository authenticationTokenRepository) {
+        super(patronRepository, authenticationTokenGenerator, authenticationTokenRepository);
+    }
+}
--- app/src/main/java/org/tomfoolery/core/usecases/admin/users/retrieval/ShowStaffAccountsUseCase.java
+++ app/src/main/java/org/tomfoolery/core/usecases/admin/users/retrieval/ShowStaffAccountsUseCase.java
@@ -1,61 +1,18 @@
 package org.tomfoolery.core.usecases.admin.users.retrieval;
 
-import lombok.Value;
-import lombok.val;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.checker.signedness.qual.Unsigned;
 import org.tomfoolery.core.dataproviders.generators.users.auth.security.AuthenticationTokenGenerator;
 import org.tomfoolery.core.dataproviders.repositories.users.StaffRepository;
 import org.tomfoolery.core.dataproviders.repositories.users.security.AuthenticationTokenRepository;
 import org.tomfoolery.core.domain.users.Staff;
-import org.tomfoolery.core.usecases.abc.AuthenticatedUserUseCase;
-import org.tomfoolery.core.utils.contracts.functional.ThrowableFunction;
-import org.tomfoolery.core.utils.dataclasses.Page;
+import org.tomfoolery.core.usecases.admin.users.retrieval.abc.ShowUsersUseCase;
 
-public final class ShowStaffAccountsUseCase extends AuthenticatedUserUseCase implements ThrowableFunction<ShowStaffAccountsUseCase.Request, ShowStaffAccountsUseCase.Response> {
-    private final @NonNull StaffRepository staffRepository;
-
+public final class ShowStaffAccountsUseCase extends ShowUsersUseCase<Staff> {
     public static @NonNull ShowStaffAccountsUseCase of(@NonNull StaffRepository staffRepository, @NonNull AuthenticationTokenGenerator authenticationTokenGenerator, @NonNull AuthenticationTokenRepository authenticationTokenRepository) {
         return new ShowStaffAccountsUseCase(staffRepository, authenticationTokenGenerator, authenticationTokenRepository);
     }
 
     private ShowStaffAccountsUseCase(@NonNull StaffRepository staffRepository, @NonNull AuthenticationTokenGenerator authenticationTokenGenerator, @NonNull AuthenticationTokenRepository authenticationTokenRepository) {
-        super(authenticationTokenGenerator, authenticationTokenRepository);
-
-        this.staffRepository = staffRepository;
+        super(staffRepository, authenticationTokenGenerator, authenticationTokenRepository);
     }
-
-    @Override
-    public @NonNull Response apply(@NonNull Request request) throws AuthenticationTokenNotFoundException, AuthenticationTokenInvalidException, PaginationInvalidException {
-        val administratorAuthenticationToken = this.getAuthenticationTokenFromRepository();
-        this.ensureAuthenticationTokenIsValid(administratorAuthenticationToken);
-
-        val pageIndex = request.getPageIndex();
-        val maxPageSize = request.getMaxPageSize();
-
-        val paginatedStaff = this.getPaginatedStaff(pageIndex, maxPageSize);
-        return Response.of(paginatedStaff);
-    }
-
-    private @NonNull Page<Staff> getPaginatedStaff(@Unsigned int pageIndex, @Unsigned int maxPageSize) throws PaginationInvalidException {
-        val paginatedStaff = this.staffRepository.showPaginated(pageIndex, maxPageSize);
-
-        if (paginatedStaff == null)
-            throw new PaginationInvalidException();
-
-        return paginatedStaff;
-    }
-
-    @Value(staticConstructor = "of")
-    public static class Request {
-        @Unsigned int pageIndex;
-        @Unsigned int maxPageSize;
-    }
-
-    @Value(staticConstructor = "of")
-    public static class Response {
-        @NonNull Page<Staff> paginatedStaff;
-    }
-
-    public static class PaginationInvalidException extends Exception {}
 }
