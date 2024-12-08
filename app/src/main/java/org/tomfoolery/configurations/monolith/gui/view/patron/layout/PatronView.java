84b229d3e0bc1ed5f75efe4a1ba1d6f83c311d6d
adnope <anhduyxh7@gmail.com>
2024-12-08 18:51:48 +0700

    Finished account center for patrons

--- app/src/main/java/org/tomfoolery/configurations/monolith/gui/view/patron/layout/PatronView.java
+++ app/src/main/java/org/tomfoolery/configurations/monolith/gui/view/patron/layout/PatronView.java
@@ -1,17 +1,14 @@
 package org.tomfoolery.configurations.monolith.gui.view.patron.layout;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 import lombok.SneakyThrows;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.tomfoolery.configurations.monolith.gui.StageManager;
 import org.tomfoolery.configurations.monolith.gui.view.abc.BaseView;
+import org.tomfoolery.configurations.monolith.gui.view.patron.actions.auth.PatronAccountCenter;
 import org.tomfoolery.configurations.monolith.gui.view.patron.actions.documents.ShowBorrowedDocumentsView;
 import org.tomfoolery.configurations.monolith.gui.view.user.scenes.DashboardView;
 import org.tomfoolery.configurations.monolith.gui.view.user.scenes.DiscoverView;
-
-import java.io.IOException;
 
 public class PatronView extends BaseView {
     @Override @SneakyThrows
@@ -41,7 +38,8 @@ public class PatronView extends BaseView {
         }
     }
 
-    private void loadDashboard() throws IOException {
+    @SneakyThrows
+    private void loadDashboard() {
         DashboardView controller = new DashboardView(
                 StageManager.getInstance().getResources().getDocumentRepository(),
                 StageManager.getInstance().getResources().getBorrowingSessionRepository(),
@@ -55,7 +53,8 @@ public class PatronView extends BaseView {
         content = dashboardLoader.load();
     }
 
-    private void loadDiscover() throws IOException {
+    @SneakyThrows
+    private void loadDiscover() {
         DiscoverView controller = new DiscoverView(
                 StageManager.getInstance().getResources().getHybridDocumentRepository(),
                 StageManager.getInstance().getResources().getDocumentSearchGenerator(),
@@ -68,7 +67,8 @@ public class PatronView extends BaseView {
         content = discoverLoader.load();
     }
 
-    private void loadShowBorrowedDocuments() throws IOException {
+    @SneakyThrows
+    private void loadShowBorrowedDocuments() {
         ShowBorrowedDocumentsView controller = new ShowBorrowedDocumentsView(
                 StageManager.getInstance().getResources().getHybridDocumentRepository(),
                 StageManager.getInstance().getResources().getDocumentContentRepository(),
@@ -82,7 +82,19 @@ public class PatronView extends BaseView {
         content = loader.load();
     }
 
+    @SneakyThrows
     private void loadAccountCenter() {
+        PatronAccountCenter controller = new PatronAccountCenter(
+                StageManager.getInstance().getResources().getPatronRepository(),
+                StageManager.getInstance().getResources().getAuthenticationTokenGenerator(),
+                StageManager.getInstance().getResources().getAuthenticationTokenRepository(),
+                StageManager.getInstance().getResources().getPasswordEncoder(),
+                StageManager.getInstance().getResources().getDocumentRepository(),
+                StageManager.getInstance().getResources().getBorrowingSessionRepository()
+        );
 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Patron/PatronAccountCenter.fxml"));
+        loader.setController(controller);
+        content = loader.load();
     }
 }
