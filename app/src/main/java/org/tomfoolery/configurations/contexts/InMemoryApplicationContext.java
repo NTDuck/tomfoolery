ea6e298b4221e668a8f96ea2e59c9e7608c0eeef
NTDuck <nguyentuduck@gmail.com>
2024-12-13 10:17:41 +0700

    Better refactored application contexts

--- app/src/main/java/org/tomfoolery/configurations/contexts/InMemoryApplicationContext.java
+++ app/src/main/java/org/tomfoolery/configurations/contexts/InMemoryApplicationContext.java
@@ -2,13 +2,9 @@ package org.tomfoolery.configurations.contexts;
 
 import lombok.val;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.tomfoolery.configurations.contexts.abc.ApplicationContext;
+import org.tomfoolery.configurations.contexts.abc.StandardApplicationContext;
 import org.tomfoolery.core.dataproviders.generators.documents.recommendation.DocumentRecommendationGenerator;
-import org.tomfoolery.core.dataproviders.generators.documents.references.DocumentQrCodeGenerator;
-import org.tomfoolery.core.dataproviders.generators.documents.references.DocumentUrlGenerator;
 import org.tomfoolery.core.dataproviders.generators.documents.search.DocumentSearchGenerator;
-import org.tomfoolery.core.dataproviders.generators.users.authentication.security.AuthenticationTokenGenerator;
-import org.tomfoolery.core.dataproviders.generators.users.authentication.security.PasswordEncoder;
 import org.tomfoolery.core.dataproviders.generators.users.search.AdministratorSearchGenerator;
 import org.tomfoolery.core.dataproviders.generators.users.search.PatronSearchGenerator;
 import org.tomfoolery.core.dataproviders.generators.users.search.StaffSearchGenerator;
@@ -20,15 +16,9 @@ import org.tomfoolery.core.dataproviders.repositories.relations.ReviewRepository
 import org.tomfoolery.core.dataproviders.repositories.users.AdministratorRepository;
 import org.tomfoolery.core.dataproviders.repositories.users.PatronRepository;
 import org.tomfoolery.core.dataproviders.repositories.users.StaffRepository;
-import org.tomfoolery.core.dataproviders.repositories.users.authentication.security.AuthenticationTokenRepository;
-import org.tomfoolery.core.dataproviders.providers.io.file.FileVerifier;
-import org.tomfoolery.infrastructures.dataproviders.generators.apache.httpclient.documents.references.CustomLandingPageDocumentUrlGenerator;
-import org.tomfoolery.infrastructures.dataproviders.generators.bcrypt.users.authentication.security.BCryptPasswordEncoder;
 import org.tomfoolery.infrastructures.dataproviders.generators.inmemory.users.search.InMemoryLinearAdministratorSearchGenerator;
 import org.tomfoolery.infrastructures.dataproviders.generators.inmemory.users.search.InMemoryLinearPatronSearchGenerator;
 import org.tomfoolery.infrastructures.dataproviders.generators.inmemory.users.search.InMemoryLinearStaffSearchGenerator;
-import org.tomfoolery.infrastructures.dataproviders.providers.io.file.apache.tika.ApacheTikaTemporaryFileStorageProvider;
-import org.tomfoolery.infrastructures.dataproviders.providers.io.file.abc.FileStorageProvider;
 import org.tomfoolery.infrastructures.dataproviders.repositories.api.rest.hathitrust.documents.HathiTrustBibliographyApiRetrievalDocumentRepository;
 import org.tomfoolery.infrastructures.dataproviders.repositories.api.rest.openlibrary.documents.OpenLibraryApiRetrievalDocumentRepository;
 import org.tomfoolery.infrastructures.dataproviders.repositories.inmemory.relations.InMemoryBorrowingSessionRepository;
@@ -36,23 +26,15 @@ import org.tomfoolery.infrastructures.dataproviders.repositories.inmemory.relati
 import org.tomfoolery.infrastructures.dataproviders.repositories.inmemory.relations.InMemoryReviewRepository;
 import org.tomfoolery.infrastructures.dataproviders.generators.inmemory.documents.recommendation.InMemoryIndexedDocumentRecommendationGenerator;
 import org.tomfoolery.infrastructures.dataproviders.generators.inmemory.documents.search.InMemoryIndexedDocumentSearchGenerator;
-import org.tomfoolery.infrastructures.dataproviders.generators.jjwt.users.authentication.security.JJWTAuthenticationTokenGenerator;
-import org.tomfoolery.infrastructures.dataproviders.generators.zxing.documents.references.ZxingDocumentQrCodeGenerator;
-import org.tomfoolery.infrastructures.dataproviders.providers.configurations.dotenv.CdimascioDotenvProvider;
-import org.tomfoolery.infrastructures.dataproviders.providers.configurations.dotenv.abc.DotenvProvider;
-import org.tomfoolery.infrastructures.dataproviders.providers.httpclient.abc.HttpClientProvider;
-import org.tomfoolery.infrastructures.dataproviders.providers.httpclient.okhttp.OkHttpClientProvider;
 import org.tomfoolery.infrastructures.dataproviders.repositories.api.rest.google.documents.GoogleApiRetrievalDocumentRepository;
-import org.tomfoolery.infrastructures.dataproviders.repositories.filesystem.users.authentication.security.KeyStoreAuthenticationTokenRepository;
 import org.tomfoolery.infrastructures.dataproviders.repositories.inmemory.documents.InMemoryDocumentRepository;
 import org.tomfoolery.infrastructures.dataproviders.repositories.inmemory.users.InMemoryAdministratorRepository;
 import org.tomfoolery.infrastructures.dataproviders.repositories.inmemory.users.InMemoryPatronRepository;
 import org.tomfoolery.infrastructures.dataproviders.repositories.inmemory.users.InMemoryStaffRepository;
-import org.tomfoolery.infrastructures.dataproviders.providers.io.file.apache.tika.ApacheTikaFileVerifier;
 
 import java.util.List;
 
-public class InMemoryApplicationContext extends ApplicationContext {
+public class InMemoryApplicationContext extends StandardApplicationContext {
     public static @NonNull InMemoryApplicationContext of() {
         return new InMemoryApplicationContext();
     }
@@ -126,51 +108,5 @@ public class InMemoryApplicationContext extends ApplicationContext {
     @Override
     protected @NonNull StaffSearchGenerator createStaffSearchGenerator() {
         return InMemoryLinearStaffSearchGenerator.of();
-    }
-
-    @Override
-    protected @NonNull DocumentQrCodeGenerator createDocumentQrCodeGenerator() {
-        return ZxingDocumentQrCodeGenerator.of();
-    }
-
-    @Override
-    protected @NonNull DocumentUrlGenerator createDocumentUrlGenerator() {
-        return CustomLandingPageDocumentUrlGenerator.of();
-    }
-
-    @Override
-    protected @NonNull AuthenticationTokenGenerator createAuthenticationTokenGenerator() {
-        return JJWTAuthenticationTokenGenerator.of();
-    }
-
-    @Override
-    protected @NonNull AuthenticationTokenRepository createAuthenticationTokenRepository() {
-        val dotenvProvider = this.getDotenvProvider();
-        return KeyStoreAuthenticationTokenRepository.of(dotenvProvider);
-    }
-
-    @Override
-    protected @NonNull PasswordEncoder createPasswordEncoder() {
-        return BCryptPasswordEncoder.of();
-    }
-
-    @Override
-    protected @NonNull DotenvProvider createDotenvProvider() {
-        return CdimascioDotenvProvider.of();
-    }
-
-    @Override
-    protected @NonNull HttpClientProvider createHttpClientProvider() {
-        return OkHttpClientProvider.of();
-    }
-
-    @Override
-    protected @NonNull FileVerifier createFileVerifier() {
-        return ApacheTikaFileVerifier.of();
-    }
-
-    @Override
-    protected @NonNull FileStorageProvider createFileStorageProvider() {
-        return ApacheTikaTemporaryFileStorageProvider.of();
     }
 }
